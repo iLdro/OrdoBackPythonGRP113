93d210aada22e86aedf03098362385133da5d6b8
Julien Klein <44435573+iLdro@users.noreply.github.com>
2023-06-26 13:56:11 +0200

    addind date and expiration

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request, Response
+import requests
 import cv2
 import numpy as np
 import firebase_admin
@@ -10,6 +11,8 @@ from pymongo import MongoClient
 from confing import MONGO_URI
 import time
 from random import randint
+import json
+import datetime
 
 app = Flask(__name__)
 
@@ -18,85 +21,32 @@ mongo_db = mongo_client["test"]
 print(mongo_db.list_collection_names()) 
 
 
-
-medecin = {
-    "id": "6489d4dce1e3c3b567b62240",
-    "nom_medecin": "Dr Philippe Langloys",
-    "intitulé": "Medecin generaliste",
-    "numero_rue": "10",
-    "nom_rue": "Pl. Robert Belvaux",
-    "code_postal": "94170",
-    "ville": "Le Perreux-sur-Marne",
-    "telephone": "+33 143244199",
-    "RPPS": "78563249587"
-}
-
-client = {
-    "id": "6489ed374c9d35917a0760f5",
-    "nom_client": "Gaillard",
-    "prenom_client": "Etienne",
-    "date": "13/07/2001"
-}
-
-medicaments = [
-    {
-        "nom_medoc": "Doliprane",
-        "dosage": "1000 mg",
-        "fréquence": "3/jour",
-        "durée": "1 semaine"
-    },
-    {
-        "nom_medoc": "Ibuprofene",
-        "dosage": "100 mg",
-        "fréquence": "1/jour",
-        "durée": "5 jours"
-    },
-    {
-        "nom_medoc": "Smecta",
-        "dosage": "500 mg",
-        "fréquence": "2/jour",
-        "durée": "4 ans"
-    },
-    {
-        "nom_medoc": "Spasfon",
-        "dosage": "100 mg",
-        "fréquence": "4/jour",
-        "durée": "pour toujours"
-    },
-    {
-        "nom_medoc": "Ketoprofene",
-        "dosage": "200 mg",
-        "fréquence": "1/8h",
-        "durée": "6 ms"
-    }
-]
-
-def generate_ordonnance():
+def generate_ordonnance(medecin, client, medicaments):
     file_name = str(int(time.time()) % randint(1, 200))
     for _ in range(5):  # Generate 5 frames
         ordonnance = np.zeros((1000, 708, 3), np.uint8)
         ordonnance.fill(255)
 
-        cv2.putText(ordonnance, medecin["nom_medecin"], (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["intitulé"], (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["numero_rue"], (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["nom_rue"], (25, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["code_postal"], (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["ville"], (60, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, medecin["telephone"], (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["name"], (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["intitule"], (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["numberStreet"], (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["street"], (25, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["postalCode"], (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["city"], (60, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["phoneNumber"], (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
         cv2.putText(ordonnance, medecin["RPPS"], (10, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
-        NomPrenom = (client["nom_client"], client["prenom_client"])
+        NomPrenom = (client["name"], client["firstname"])
         Id_client = " ".join(NomPrenom)
         cv2.putText(ordonnance, Id_client, (355, 225), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(ordonnance, client["date"], (355, 245), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, client["dateNaissance"], (355, 245), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
         coord = (75, 340)
         for medicament in medicaments:
-            nom = medicament["nom_medoc"]
+            nom = medicament["nom_medicament"]  # Updated parameter name
             dose = medicament["dosage"]
             freq = medicament["fréquence"]
-            temps = medicament["durée"]
+            temps = medicament["duree"]  # Updated parameter name
 
             cv2.putText(ordonnance, nom, coord, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
             infos_medocs = (dose, freq, temps)
@@ -107,7 +57,7 @@ def generate_ordonnance():
             cv2.line(ordonnance, coord, (coord[0] + 400, coord[1]), (0, 0, 0), 1)
             coord = (coord[0], coord[1] + 30)
 
-        cv2.putText(ordonnance, medecin["nom_medecin"], (350, 900), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(ordonnance, medecin["name"], (350, 900), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
         ret, buffer = cv2.imencode('.jpg', ordonnance)
         frame = buffer.tobytes()
@@ -115,6 +65,7 @@ def generate_ordonnance():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     return file_name
+
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("odomastercamp-firebase-adminsdk-jdqqe-2be3aebde8.json")
@@ -141,10 +92,35 @@ def save_image_to_firebase(image_data):
     return url
 
 
-@app.route('/ordonnance')
+@app.route('/ordonnance', methods=['POST'])
 def ordonnance():
+    print(request)
+    if request.method == 'POST': 
+        request_data = request.get_json()
+        medecin_id = request_data['medecin_id']
+        user_id = request_data['client_id']
+        medicaments = request_data['medicaments']
+        
+    express_url = "http://localhost:5000"
+    
+    try:
+        response = requests.post(express_url + "/medById", json={"id": medecin_id})
+        print("medecin", response.text)  # Affiche la réponse JSON reçue
+        medecin = response.json()  # Convertit la réponse JSON en objet Python
+        client = requests.post("http://localhost:5000/med/getUserById", json={"id": user_id}).json()
+        print(medecin)
+        print(client)
+    except requests.exceptions.RequestException as e:
+        print(str(e))
+        return "erreur"
+
+    print(request_data)
+    
+    
+    
+        
     mongo_collection = mongo_db["ordonnances"]
-    image_data = generate_ordonnance()
+    image_data = generate_ordonnance(medecin, client, medicaments)
     image_bytes = next(image_data)  # Get the first generated frame as bytes
     image_data.close()  # Close the generator to release resources
 
@@ -152,14 +128,18 @@ def ordonnance():
 
     ordonnance_data = {
         'image_url': image_url,
-        'medecin_id': medecin["id"],
-        'user_id': client["id"],
-        'medicaments': medicaments
+        'medecin_id': medecin_id,
+        'user_id': user_id,
+        'medicaments': medicaments,
+        'pharmacien': " ",
+        'dateDeCréation': datetime.datetime.now(),
+        'expired': False,
     }
 
     result = mongo_collection.insert_one(ordonnance_data)
 
     print('Created ordonnance {0}'.format(result.inserted_id))
+    return jsonify(request_data)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5173)
